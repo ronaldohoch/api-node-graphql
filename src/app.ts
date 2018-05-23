58280a3817a9119b14f3667e0386f1346acbcc6a
Ronaldo Hoch <ronaldo.hoch@softdesign-rs.com.br>
2018-05-23 15:42:54 -0300

    Implementação GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as graphqlHTTP from 'express-graphql';
+import schema from './graphql/schema';
 
 class App{
     public express: express.Application;
@@ -9,12 +11,10 @@ class App{
     }
 
     private middleware(): void{
-        console.log("middleware")
-        this.express.use('/hello',(req:express.Request, res: express.Response, next:express.NextFunction)=>{
-            res.send({
-                hello:"Olá"
-            });
-        });
+        this.express.use('/graphql',graphqlHTTP({
+            schema:schema,
+            graphiql:true
+        }));
     }
 }
 
